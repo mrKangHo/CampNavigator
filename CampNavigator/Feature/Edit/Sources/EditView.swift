ebe4f1c7692d41f49146b87927873daa4d3e6ade
Kano <coke8707@gmail.com>
2024-07-29 22:10:58 +0900

    feat : EditView Layout 구성중

--- CampNavigator/Feature/Edit/Sources/EditView.swift
+++ CampNavigator/Feature/Edit/Sources/EditView.swift
@@ -8,18 +8,82 @@
 
 import SwiftUI
 import KHDesignSystem
+import Resources
+
+struct ListItem: Identifiable {
+    let id = UUID()
+    let icon: String
+    let text: String
+}
 
 public struct EditView: View {
     public init() {}
+    @State public var name:String = ""
+    @State public var visitDate:Date = Date()
+    @State public var items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+    @State private var showAlert = false
+    @State private var selectedIndex: Int? = nil // 선택된 인덱스
+       
+    let  tags: [ListItem] = [
+            ListItem(icon: "house.fill", text: "Home"),
+            ListItem(icon: "gearshape.fill", text: "Settings"),
+            ListItem(icon: "person.fill", text: "Profile"),
+            ListItem(icon: "bell.fill", text: "Notifications"),
+            ListItem(icon: "star.fill", text: "Favorites")
+        ]
+    
     public  var body: some View {
         VStack {
-            Text("Detail for")
-                .font(KHFont.custom(.Bold, size: 40))
-            // 여기에 더 많은 상세 정보 UI를 추가할 수 있습니다.
-        }
+             KHTextField(text: $name, 
+                         placeHolder: "캠핑장의 이름은 어떻게 되나요?",
+                         symboldIcon:Image(systemName: "pencil"))
+            DatePicker("방문은 언제했나요?", selection: $visitDate)
+            
+            KHTextField(text: $name, 
+                        placeHolder: "지역은 어디인가요?",
+                        symboldIcon:Image(systemName: "pencil"))
+            KHTextField(text: $name, 
+                        placeHolder: "시설은?",
+                        symboldIcon:Image(systemName: "pencil"))
+            
+            
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 16) {
+                    ForEach(items.indices, id: \.self) { index in
+                        PhotoItemView(Photo: ResourcesAsset.sample.swiftUIImage) {
+                            print("LEE")
+                            selectedIndex = index
+                            showAlert = true
+                        }
+                    }
+                }.alert("사진을 삭제할까요?", isPresented: $showAlert) {
+                    
+                    Button("삭제", role: .destructive) {
+                        if let index = selectedIndex {
+                            items.remove(at: index)
+                        }
+                    }
+                    Button("취소", role: .cancel) {
+                        
+                    }
+                }
+                .padding()
+                
+            }
+            Spacer()
+            KHButton(title: "확인", action: {
+                
+            }, KHButtonStyle(background: KHColor.Primary.P00, 12)).foregroundColor(.white).frame(height: 50)
+        }.padding()
         .navigationBarTitleDisplayMode(.inline)
     }
 }
+
+
+
+
+
 
 #Preview {
     EditView()
---
+++ CampNavigator/Feature/Edit/Sources/PhotoItemView.swift
@@ -0,0 +1,48 @@
+//
+//  PhotoItemView.swift
+//  Edit
+//
+//  Created by LEE on 7/29/24.
+//  Copyright © 2024 COKE. All rights reserved.
+//
+
+import SwiftUI
+import Resources
+import KHDesignSystem
+
+public struct PhotoItemView: View {
+    public var Photo:Image
+    public var onDelete:() -> Void
+    
+    public var body: some View {
+        
+        ZStack(alignment: .topTrailing) {
+            Photo.resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 140, height: 140)
+                .clipped()
+                .cornerRadius(8).onTapGesture {
+                    print("KANGHO")
+                    onDelete()
+                }
+//            Button {
+//                
+//            } label: {
+//                Image(systemName: "xmark.circle").resizable()
+//                    .foregroundColor(KHColor.Gray.GR10)
+//                    .frame(width: 14, height: 14)
+//                    .padding(6)
+//                    .background(KHColor.Gray.GR90.opacity(0.7))
+//                    .cornerRadius(8)
+//            }
+        }
+        
+        
+    }
+}
+
+#Preview {
+    PhotoItemView(Photo: ResourcesAsset.sample.swiftUIImage) {
+        
+    }
+}
